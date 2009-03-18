b5e5eea53ce4a0325afaf759a810241acf838820
James Blackburn <jamesblackburn@gmail.com>
2009-03-18 23:56:07 +0000

    Add eclipse-core-contenttype

---
+++ scripts/eclipse-sync/eclipse-core-contenttype-cvsimport.sh
@@ -0,0 +1,2 @@
+git cvsimport -i -v -C eclipse-core-contenttype -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse -r cvs org.eclipse.core.contenttype
+git cvsimport -i -v -C eclipse-core-contenttype -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse -r cvs_e4 e4/org.eclipse.e4.resources/bundles/org.eclipse.core.contenttype
